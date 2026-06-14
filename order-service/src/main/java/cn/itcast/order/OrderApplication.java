59dfee5aee2f1e1447593b6362cf0b43f0d21b01
ljj <1412415464@qq.com>
2026-06-14 10:56:46 +0800

    更改feign目录结构，统一存放方便管理。

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableFeignClients(basePackages = "cn.itcast.order.service")
+@EnableFeignClients(basePackages = "cn.itcast.feignApi.clients")
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
